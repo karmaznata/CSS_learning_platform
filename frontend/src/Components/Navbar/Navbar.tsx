485b9ccddb629b7418b394cfbeda0c71f9689c92
Nataliia Karmazina <natalia.karmazina.ipz@gmail.com>
2024-04-09 07:32:36 +0200

    new

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -66,7 +66,7 @@ const Navbar = (prop) => {
                 <img src={adessoLogo} alt="company logo" />
             </div>
             <ul className="nav-menu nav nav-underline fs-6">
-                <li className="nav-item" role="button" onClick={() => {handleMenuClick("homePage") }} >
+                <li className="nav-item" role="button" onClick={() => {handleMenuClick("homePage") }} onKeyDown={() => {}}>
                     <Link to='/' className={`nav-link ${menu === "homePage" ? "active" : ""}`}>Home Page</Link>
                 </li>
                 <div className="tutorial-menu-dropdown"
@@ -74,7 +74,7 @@ const Navbar = (prop) => {
                     onMouseLeave={() => setDropdownVisible(false)}
                     role="button"
                 >
-                    <li className="nav-item" role="button" onClick={() => {handleMenuClick("tutorials") }} >
+                    <li className="nav-item" role="button" onClick={() => {handleMenuClick("tutorials") }} onKeyDown={() => {}}>
                         <Link to='/tutorials' className={`nav-link ${menu === "tutorials" ? "active" : ""}`} >Tutorials</Link>
                     </li>
                     {/* {isDropdownVisible && <TutorialsMenu handleMenuClick={handleMenuClick}/>} */}
