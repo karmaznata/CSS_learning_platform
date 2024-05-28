12a32d6160084837c46b443ea168714c04ce360d
Nataliia Karmazina <natalia.karmazina.ipz@gmail.com>
2024-05-28 18:22:39 +0200

    attempt23

--- backend/index.js
+++ backend/index.js
@@ -76,7 +76,7 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+    sameSite: 'none'
   }
 }));
 
